94aba8e722cf35693f40f268933a9e3d8ea2c0d1
Augussst <oka.august@gmail.com>
2023-03-27 08:37:28 +0700

    add page navigation component

---
+++ src/lib/components/PageNav.svelte
@@ -0,0 +1,47 @@
+<script>
+    export let back
+    export let next
+    import { Button } from 'flowbite-svelte'
+</script>
+
+<div class="flex justify-around">
+	<Button
+		href={back}
+		class="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6 mr-1"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
+			/>
+		</svg>
+		Kembali
+	</Button>
+	<Button
+		href={next}
+		class="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
+	>
+		Selanjutnya <svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6 ml-1"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+			/>
+		</svg>
+	</Button>
+</div>
--- src/routes/courses/1.1.-asal-hukum-syariah/+page.svelte
+++ src/routes/courses/1.1.-asal-hukum-syariah/+page.svelte
@@ -2,6 +2,7 @@
 	import { Breadcrumb, BreadcrumbItem, Button } from 'flowbite-svelte';
 	import Parag from '$lib/components/Parag.svelte';
 	import Iframe from '$lib/components/Iframe.svelte';
+	import PageNav from '$lib/components/PageNav.svelte';
 </script>
 
 <Breadcrumb class="mb-5" aria-label="Solid background breadcrumb" solid>
@@ -23,44 +24,4 @@
 
 <Iframe title="interactive1" src="https://augussst.github.io/AS-LuasdanVolume-BangunRuang/"></Iframe>
 
-<div class="flex justify-around">
-	<Button
-		href="/"
-		class="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
-	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-6 h-6 mr-1"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
-			/>
-		</svg>
-		Kembali
-	</Button>
-	<Button
-		href="/"
-		class="bg-green-500 hover:bg-green-600 dark:bg-green-500 dark:hover:bg-green-600"
-	>
-		Selanjutnya <svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-6 h-6 ml-1"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-			/>
-		</svg>
-	</Button>
-</div>
+<PageNav back="/courses" next="/1.2.-prinsip-bank-syariah"></PageNav>
