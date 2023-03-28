74f1ff20cb2efd07e170ab331110fd67c4a8388f
Augussst <oka.august@gmail.com>
2023-03-28 11:46:05 +0700

    add components to get the page titile (<head>)

---
+++ src/lib/utils/HeadPageTitle.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { page } from '$app/stores'
+
+    $: pageTitle = () => {
+        // Use status code if there's an error
+        if ($page.error) return $page.status
+
+        if ($page.url.pathname === '/') return 'Home'
+
+        const title = $page.url.pathname.split('/').pop().replace(/-/g, ' ')
+        const capilized = title.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
+
+        return String(capilized) || ''
+    }
+</script>
+
+<svelte:head>
+    <title>SBL - {pageTitle()}</title>
+</svelte:head>
