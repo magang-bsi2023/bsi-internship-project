e4635e3a0aa9d5ebcfc43c43f3429a43b063bb2d
Augussst <oka.august@gmail.com>
2023-03-28 11:45:10 +0700

    add layout for materi pages

---
+++ src/lib/components/MateriLayout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import PageNav from '$lib/components/PageNav.svelte';
+	import BcCourse from '$lib/components/BcCourse.svelte';
+	import PageTitle from '$lib/utils/PageTitle.svelte';
+    export let next
+    export let prev
+</script>
+
+<BcCourse></BcCourse>
+
+<h1 class="capitalize text-2xl text-green-500 font-bold dark:text-white mb-1">
+	<PageTitle></PageTitle>
+</h1>
+
+<slot></slot>
+
+<PageNav back={prev} next={next}></PageNav>
