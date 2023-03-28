9db164448e1fcc041cc54490817314ab979e38a8
Augussst <oka.august@gmail.com>
2023-03-28 11:44:41 +0700

    add breadcrumb component for course

---
+++ src/lib/components/BcCourse.svelte
@@ -0,0 +1,10 @@
+<script>
+    import {Breadcrumb, BreadcrumbItem} from 'flowbite-svelte';
+    import PageTitle from '$lib/utils/PageTitle.svelte';
+</script>
+
+<Breadcrumb class="mb-5" aria-label="Solid background breadcrumb" solid>
+    <BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+    <BreadcrumbItem href="/materi">Materi</BreadcrumbItem>
+    <BreadcrumbItem><PageTitle></PageTitle></BreadcrumbItem>
+</Breadcrumb>
