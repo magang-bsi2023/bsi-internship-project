aa7dc9c963d285daba0acce0b7c9fe5b4e631842
Augussst <oka.august@gmail.com>
2023-03-29 14:36:54 +0700

    add component for google form embed

---
+++ src/lib/components/Evaluasi.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let src;
+</script>
+
+<div class="border rounded-lg border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 py-5 px-2">
+    <div class="relative w-full h-screen">
+        <iframe title="evaluasi" class="absolute w-full h-full" src={src}>Loading…</iframe>
+    </div>
+</div>
