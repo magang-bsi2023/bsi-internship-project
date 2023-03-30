80848eda0426eb1a3cc0455a81192e1eef662ab7
Augussst <oka.august@gmail.com>
2023-03-30 14:19:08 +0700

    add page title and tagline script

---
+++ src/routes/+layout.js
@@ -0,0 +1,11 @@
+/** @type {import('./$types').PageLoad} */
+
+// eslint-disable-next-line no-unused-vars
+export function load({ params }) {
+    return {
+        page: {
+            title: "BSI-Web-Project",
+            tagline: "ini tagline",
+        }
+    };
+}
