0350a10027d6baf54d471afc53103ae1050c97a5
Augussst <oka.august@gmail.com>
2023-03-30 09:03:50 +0700

    add margin bottom

--- src/lib/components/Evaluasi.svelte
+++ src/lib/components/Evaluasi.svelte
@@ -2,7 +2,7 @@
     export let src;
 </script>
 
-<div class="border rounded-lg border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 py-5 px-2">
+<div class="border rounded-lg border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 py-5 px-2 mb-5">
     <div class="relative w-full h-screen">
         <iframe title="evaluasi" class="absolute w-full h-full" src={src}>Loading…</iframe>
     </div>
