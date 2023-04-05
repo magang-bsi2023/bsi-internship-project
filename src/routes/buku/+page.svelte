389d3dd87a04775c71c7ada3ed38a752365952bf
Augussst <oka.august@gmail.com>
2023-04-05 10:32:43 +0700

    add img path to function

--- src/routes/buku/+page.svelte
+++ src/routes/buku/+page.svelte
@@ -1,27 +1,40 @@
 <script>
-	import { Heading, A } from "flowbite-svelte";
-	import { page } from '$app/stores';
+	import { Heading, Card, P } from "flowbite-svelte";
 	import Breadcrumbs from "$lib/components/Breadcrumbs.svelte";
 
 	function getBuku() {
-		const modules = import.meta.glob("$buku/*/+page.svelte");
-		let body = [];
-		for (let path in modules) {
-			let pathSanitized = path.replace("/+page.svelte", "").replace("/src/routes/", "/");
-			body.push({
+	const modules = import.meta.glob(["$buku/*/+page.svelte", "$lib/assets/book-cover/*"]);
+	const buku = [];
+
+	for (let path in modules) {
+		const pathSanitized = path.replace("/+page.svelte", "").replace("/src/routes/", "/");
+		const imgPath = pathSanitized.replace("/buku/", "src/lib/assets/book-cover/").concat(".jpg");
+		const isSvelteFile = path.endsWith("+page.svelte");
+
+		if (isSvelteFile) {
+			buku.push({
 				title: pathSanitized.replace("/buku/", "").replace(/-/g, " "),
-				link: pathSanitized
-			});
-		}
-		return body;
+				link: pathSanitized,
+				img: imgPath
+			})
+		} 
 	}
 
+	return buku;
+	}
+
+	console.log(getBuku());
 </script>
 
 <Breadcrumbs bcClass="mb-5"></Breadcrumbs>
 
-<Heading>Kumpulan Buku</Heading>
-
-{#each getBuku() as buku}
-	<A href={buku.link}>{buku.title}</A>
-{/each}
+<Heading tag="h1" class="mb-5">Daftar Buku</Heading>
+<div class="grid grid-rows-2 gap-3">
+	{#each getBuku() as buku}
+		<div>
+			<Card horizontal size="full" href={buku.link} img={buku.img}>
+				<Heading tag="h2" class="capitalize">{buku.title}</Heading>
+			</Card>
+		</div>
+	{/each}
+</div>
