bf5546122bc28fc94c014d57c0f0d5fec1caa260
Augussst <oka.august@gmail.com>
2023-04-06 13:48:14 +0700

    redesign front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,33 @@
 <script>
-	import { Heading, Accordion, AccordionItem, Button } from 'flowbite-svelte';
+	import { Heading, Button, P, Input, Label, ButtonGroup, InputAddon } from 'flowbite-svelte';
 	import logo from '$lib/assets/logo/bsilib.svg';
 	export let data;
 </script>
 
 <div class="flex flex-col p-5 text-center my-5">
 	<img src={logo} class="h-32 mb-3" alt="{data.page.title} Logo" />
-	<Heading tag="h1" customSize="text-4xl font-semibold" color="text-bsiyellow" class="mb-5">
+	<Heading tag="h1" customSize="text-4xl font-semibold" color="text-bsiyellow">
 		{data.page.title}
 	</Heading>
-	<!-- <P class="dark:text-gray-400 text-center capitalize">{data.page.tagline}</P> -->
 </div>
 
-<div class="flex">
-	<Button href="/buku" color="bg-bsigreen" class="mx-auto bg-bsigreen">
-		Daftar Buku
+<P class="text-xl">
+	Kumpulan buku BSI dan buku perbankan syariah dengan media interaktif
+</P>
+
+<div class="my-5">
+	<ButtonGroup class="w-full">
+	<InputAddon><svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg></InputAddon>
+	<Input size="lg" defaultClass="block w-full disabled:cursor-not-allowed disabled:opacity-50 focus:border-bsigreen focus:ring-bsigreen dark:focus:border-bsigreen dark:focus:ring-bsigreen bg-gray-50 text-gray-900 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 border-gray-300 dark:border-gray-600 p-4 sm:text-base size first:rounded-l-lg last:rounded-r-lg border-l-0 first:border-l last:border-r" placeholder="Cari buku" />
+	<Button color="" class="bg-bsigreen text-white hover:bg-bsidgreen">
+		Cari
+	</Button>
+	</ButtonGroup>
+</div>
+
+<div class="flex flex-col items-center">
+	<P class="mb-5">atau</P>
+	<Button pill size="lg" href="/buku" color="" class="mx-auto border-2 border-bsigreen bg-bsigreen hover:bg-bsidgreen hover:border-bsiyellow text-white">
+		Lihat Daftar Buku
 	</Button>
 </div>
