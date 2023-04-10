456ca21788edac390751a7698bd2da882a7e3e5d
Augussst <oka.august@gmail.com>
2023-04-10 13:14:21 +0700

    add no result indicator

--- src/routes/buku/+page.svelte
+++ src/routes/buku/+page.svelte
@@ -29,18 +29,22 @@
 </div>
 
 <div class="flex flex-col flex-wrap md:flex-row md:justify-evenly gap-5 md:gap-y-10">
-	{#each $searchStore.filtered as buku}
-		<a href={buku.link} class="flex gap-3 md:inline md:w-fit [&>div]:hover:bg-gray-100 [&>div]:dark:hover:bg-gray-700">
-			<div class="relative h-auto w-32 md:h-[30rem] md:w-fit md:mx-auto overflow-hidden rounded-lg shadow-xl">
-				<!-- <div class="blur-sm h-full w-full absolute bg-center bg-no-repeat -z-10" style="background-image: url({buku.img});"></div> -->
-				<img class="h-full mx-auto" src={buku.img} alt="{buku.title} cover">
-			</div>
-			<div class="grow flex items-center md:items-start md:mt-3 py-2 px-3 bg-gray-50 dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 shadow-md">
-				<div class="md:mx-auto md:[&>*]:text-center">
-					<Heading tag="h2" customSize="text-2xl md:text-xl" class="capitalize">{buku.title}</Heading>
-					<P color="text-gray-400">This is book description</P>
+	{#if $searchStore.filtered.length}
+		{#each $searchStore.filtered as buku}
+			<a href={buku.link} class="flex gap-3 md:inline md:w-fit [&>div]:hover:bg-gray-100 [&>div]:dark:hover:bg-gray-700">
+				<div class="relative h-auto w-32 md:h-[30rem] md:w-fit md:mx-auto overflow-hidden rounded-lg shadow-xl">
+					<!-- <div class="blur-sm h-full w-full absolute bg-center bg-no-repeat -z-10" style="background-image: url({buku.img});"></div> -->
+					<img class="h-full mx-auto" src={buku.img} alt="{buku.title} cover">
 				</div>
-			</div>
-		</a>
-	{/each}
+				<div class="grow flex items-center md:items-start md:mt-3 py-2 px-3 bg-gray-50 dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 shadow-md">
+					<div class="md:mx-auto md:[&>*]:text-center">
+						<Heading tag="h2" customSize="text-2xl md:text-xl" class="capitalize">{buku.title}</Heading>
+						<P color="text-gray-400">This is book description</P>
+					</div>
+				</div>
+			</a>
+		{/each}
+	{:else}
+		<P class="mt-5">Tidak ada buku dengan judul "{$searchStore.search}"</P>
+	{/if}
 </div>
