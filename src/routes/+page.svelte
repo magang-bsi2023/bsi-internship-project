c819439f31dde93ca2f1206055a6a2f9cbac422c
Augussst <oka.august@gmail.com>
2023-04-11 14:15:40 +0700

    remove unused stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { Heading, Button, P, Input, Label, ButtonGroup, InputAddon } from 'flowbite-svelte';
-	import { goto } from '$app/navigation';
 	import logo from '$lib/assets/logo/bsilib.svg';
 	export let data;
 </script>
@@ -12,12 +11,8 @@
 	</Heading>
 </div>
 
-<!-- <P class="text-xl">
-	Kumpulan buku BSI dan buku perbankan syariah dengan media interaktif
-</P> -->
-
 <div class="my-5">
-	<form method="GET" action="/buku"> <!-- on:submit|preventDefault={onSubmit} -->
+	<form method="GET" action="/buku">
 		<ButtonGroup class="w-full">
 		<InputAddon><svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg></InputAddon>
 		<Input size="lg" type="search" name="q" placeholder="Cari buku..."
@@ -28,8 +23,6 @@
 		</ButtonGroup>
 	</form>
 </div>
-
-
 
 <div class="flex flex-col items-center">
 	<P class="mb-5">atau</P>
