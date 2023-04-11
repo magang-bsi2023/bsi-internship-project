04178525fb564287d38ccae5c29bab9c742dad1f
Augussst <oka.august@gmail.com>
2023-04-11 12:00:48 +0700

    adjust search bar

--- src/routes/buku/+page.svelte
+++ src/routes/buku/+page.svelte
@@ -38,11 +38,11 @@
 
 <Breadcrumbs bcClass="mb-5"></Breadcrumbs>
 
-<div class="flex mb-5">
-	<Heading tag="h1" customSize="text-3xl" color="text-bsigreen" class="font-bold">Daftar Buku</Heading>
-	<div class="flex flex-row items-center px-2 bg-gray-50 dark:bg-gray-800 border-2 border-gray-200 dark:border-gray-700 focus-within:border-bsiyellow focus-within:dark:border-bsiyellow rounded-full">
+<div class="flex justify-between mb-5">
+	<Heading class="font-bold w-fit whitespace-nowrap mr-5" tag="h1" customSize="text-3xl" color="text-bsigreen">Daftar Buku</Heading>
+	<div class="flex w-1/2 flex-row items-center px-2 bg-gray-50 dark:bg-gray-800 border-2 border-gray-200 dark:border-gray-700 focus-within:border-bsiyellow focus-within:dark:border-bsiyellow rounded-full">
 		<svg class="w-7 h-7 stroke-bsiyellow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg>
-		<input class="-ml-2 border-0 focus:ring-0 bg-transparent dark:text-white" 
+		<input class="-ml-2 w-full border-0 focus:ring-0 bg-transparent dark:text-white" 
 		type="search" placeholder="Cari buku..." bind:value={$searchStore.search}>
 	</div>
 </div>
@@ -52,18 +52,16 @@
 		{#each $searchStore.filtered as buku}
 			<a href={buku.link} class="flex gap-3 md:inline md:w-fit [&>div]:hover:bg-gray-100 [&>div]:dark:hover:bg-gray-700">
 				<div class="relative h-auto w-32 md:h-[30rem] md:w-fit md:mx-auto overflow-hidden rounded-lg shadow-xl">
-					<!-- <div class="blur-sm h-full w-full absolute bg-center bg-no-repeat -z-10" style="background-image: url({buku.img});"></div> -->
 					<img class="h-full mx-auto" src={buku.img} alt="{buku.title} cover">
 				</div>
 				<div class="grow flex items-center md:items-start md:mt-3 py-2 px-3 bg-gray-50 dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 shadow-md">
 					<div class="md:mx-auto md:[&>*]:text-center">
 						<Heading tag="h2" customSize="text-2xl md:text-xl" class="capitalize">{buku.title}</Heading>
-						<P color="text-gray-400">This is book description</P>
 					</div>
 				</div>
 			</a>
 		{/each}
 	{:else}
-		<P class="mt-5">Tidak ada buku dengan judul "{$searchStore.search}"</P>
+		<P class="mt-5 mx-auto">Tidak ada buku dengan judul "{$searchStore.search}"</P>
 	{/if}
 </div>
