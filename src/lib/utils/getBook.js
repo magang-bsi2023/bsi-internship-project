51b20fc40cfb2f51c690d0160889251e609049c7
Augussst <oka.august@gmail.com>
2023-04-11 10:07:31 +0700

    add function to get bookUnit

--- src/lib/utils/getBook.js
+++ src/lib/utils/getBook.js
@@ -11,9 +11,30 @@ export function getBuku() {
 			buku.push({
 				title: pathSanitized.replace("/buku/", "").replace(/-/g, " "),
 				link: pathSanitized,
-				img: imgPath
+				img: imgPath,
 			})
 		} 
 	}
 	return buku;
+}
+
+export function getBukuUnit() {
+	const modules = import.meta.glob("$buku/*/*/+page.svelte");
+	const bukuUnit = [];
+
+	for (let path in modules) {
+		const pathSanitized = path.replace("/src/routes/", "/").replace("/+page.svelte", "");
+		const bookTitle = pathSanitized.replace(/-/g, " ").split("/").slice(2, 3).toString()
+		const title = pathSanitized.replace(/-/g, " ").split("/").slice(3).toString()
+
+		console.log(bookTitle);
+
+		bukuUnit.push({
+			bookTitle: bookTitle,
+			title: title,
+			link: pathSanitized
+		})
+
+	}
+	return bukuUnit
 }
