2d3cf68355c3a7e60c953b7b4396ad9616e73f7e
Augussst <oka.august@gmail.com>
2023-04-12 13:05:13 +0700

    insert video to video tab

--- src/routes/buku/energi-baru-untuk-indonesia/1.1.-barter-dan-uang/+page.svelte
+++ src/routes/buku/energi-baru-untuk-indonesia/1.1.-barter-dan-uang/+page.svelte
@@ -2,6 +2,7 @@
 	import { P } from 'flowbite-svelte'
 	import MateriLayout from '$lib/components/MateriLayout.svelte';
 	import Iframe from '$lib/components/Iframe.svelte';
+	import Viframe from '$lib/components/Viframe.svelte';
 </script>
 
 <MateriLayout linkEvaluasi="https://docs.google.com/forms/d/e/1FAIpQLSfzgmovxA_6aUeELAdrXzDiHhzSPmni25S11R1DZaaQ1cAHdQ/viewform?embedded=true">
@@ -10,8 +11,9 @@
 		reprehenderit inventore. Placeat unde quibusdam voluptas cum quam? Ut soluta, explicabo numquam
 		neque quidem placeat cupiditate fuga?
 	</P>
-	
-	<Iframe title="video1" src="https://drive.google.com/file/d/1-9ZKjicBRw0ysRC_sjFhi0BUonOz0wIu/preview"></Iframe>
+	<span slot="video">
+		<Viframe title="video1" src="https://drive.google.com/file/d/1Otk0Epy-BYsph8ZHUAlT95Lcet7b8_xQ/preview"></Viframe>
+	</span>
 	
 	<Iframe title="interactive1" src="https://augussst.github.io/AS-LuasdanVolume-BangunRuang/"></Iframe>
 </MateriLayout>
